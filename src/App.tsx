ca469954ee77f640f7127538e7d2feaa6a3a9753
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-16 21:57:57 +0300

    feat: integrate Header into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Header } from "./react/components/layouts/Header/Header";
+
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <Header />
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
