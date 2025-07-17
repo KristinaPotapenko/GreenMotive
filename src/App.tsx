f3e00d8571cd4a703063a4f9882fce5a37e64e00
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-17 21:30:02 +0300

    feat: add Footer component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Header } from "./react/components/layouts/Header/Header";
+import { Footer } from "./react/components/layouts/Footer/Footer";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
+      <main></main>
+      <Footer />
     </div>
   );
 }
