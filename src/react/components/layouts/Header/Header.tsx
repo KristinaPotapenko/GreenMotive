52e4d8e13adcbb345ecae200d986929eeb58aa09
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-18 21:28:23 +0300

    feat: add responsive styles to Header component

--- src/react/components/layouts/Header/Header.tsx
+++ src/react/components/layouts/Header/Header.tsx
@@ -5,22 +5,28 @@ import { Navigation } from "./Navigation/Navigation";
 import { NavigationLink } from "../../ui/NavigationLink/NavigationLink";
 
 import styles from "./Header.module.scss";
+import { useWindowWidth } from "../../../../scripts/hooks/useWindowWidth";
 
 export const Header = () => {
+  const windowWidth = useWindowWidth();
+  const isTablet = windowWidth <= 1023;
+
   return (
     <header className={cl(styles.header, "container")}>
-      <Logo />
+      {!isTablet && <Logo />}
       <Navigation />
-      <NavigationLink path="/">
-        <svg>
-          <use
-            xlinkHref={`${
-              import.meta.env.BASE_URL
-            }assets/icons/sprite.svg#lightning`}
-          />
-        </svg>
-        Renewable Energy Solutions
-      </NavigationLink>
+      {!isTablet && (
+        <NavigationLink path="/">
+          <svg>
+            <use
+              xlinkHref={`${
+                import.meta.env.BASE_URL
+              }assets/icons/sprite.svg#lightning`}
+            />
+          </svg>
+          Renewable Energy Solutions
+        </NavigationLink>
+      )}
     </header>
   );
 };
--- src/react/components/layouts/Header/Navigation/Navigation.tsx
+++ src/react/components/layouts/Header/Navigation/Navigation.tsx
@@ -2,8 +2,13 @@ import { Link } from "react-router-dom";
 import cl from "classnames";
 
 import styles from "../Header.module.scss";
+import { useWindowWidth } from "../../../../../scripts/hooks/useWindowWidth";
+import { Logo } from "../../../Logo/Logo";
 
 export const Navigation = () => {
+  const windowWidth = useWindowWidth();
+  const isTablet = windowWidth <= 1023;
+
   return (
     <ul className={styles.headerList}>
       <li className={cl(styles.headerItem, styles.headerItemActive)}>
@@ -20,9 +25,13 @@ export const Navigation = () => {
         <nav></nav>
       </li>
       <li className={styles.headerItem}>
-        <Link className={styles.headerLink} to="/">
-          Discover Innovation
-        </Link>
+        {isTablet ? (
+          <Logo />
+        ) : (
+          <Link className={styles.headerLink} to="/">
+            Discover Innovation
+          </Link>
+        )}
       </li>
     </ul>
   );
