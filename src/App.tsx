909255769ae374a7d0a8ba272a6a7a234c30768c
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-20 20:53:22 +0300

    feat: add Home page in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+
 import { Header } from "./react/components/layouts/Header/Header";
+import { Home } from "./react/pages/Home/Home";
 import { Footer } from "./react/components/layouts/Footer/Footer";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
-      <main></main>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
+      </main>
       <Footer />
     </div>
   );
