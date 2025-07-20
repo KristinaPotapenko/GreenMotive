7cc010d1887b0ef396daa8d460556e60a5f79501
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-20 20:52:28 +0300

    feat: create Home page

---
+++ src/react/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import { Hero } from "../../sections/Hero/Hero";
+import { Mission } from "../../sections/Mission/Mission";
+import { Priorities } from "../../sections/Priorities/Priorities";
+import { Vision } from "../../sections/Vision/Vision";
+
+export const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Mission />
+      <Priorities />
+      <Vision />
+    </>
+  );
+};
