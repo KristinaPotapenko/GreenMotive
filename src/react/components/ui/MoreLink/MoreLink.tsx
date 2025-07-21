4ea986fb13e7572a9692d2f4f7a62c237008de5f
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-21 20:54:53 +0300

    feat: add GSAP reveal animation to MoreLink component

--- src/react/components/ui/MoreLink/MoreLink.tsx
+++ src/react/components/ui/MoreLink/MoreLink.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./MoreLink.module.scss";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 interface MoreLinkProps {
   path: string;
@@ -9,8 +11,26 @@ interface MoreLinkProps {
 }
 
 export const MoreLink = ({ path, children }: MoreLinkProps) => {
+  const linkRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      linkRef.current,
+      {
+        opacity: 0,
+        y: -40,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 0.2,
+        duration: 1.5,
+      }
+    );
+  }, []);
+
   return (
-    <Link to={path} className={styles.link}>
+    <Link ref={linkRef} to={path} className={styles.link}>
       {children}
     </Link>
   );
