303a8cda0951952257ee48d92dfe22b2415ba64f
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-21 20:56:00 +0300

    feat: add GSAP reveal animation to Logo component

--- src/react/components/Logo/Logo.tsx
+++ src/react/components/Logo/Logo.tsx
@@ -3,11 +3,24 @@ import { Link } from "react-router-dom";
 import { scrollToTop } from "../../../scripts/helpers/scrollToTop";
 
 import styles from "./Logo.module.scss";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 export const Logo = () => {
+  const logoIconRef = useRef(null);
+
+  useEffect(() => {
+    gsap.to(logoIconRef.current, {
+      opacity: 0.5,
+      rotate: 180,
+      delay: 0.2,
+      duration: 2,
+    });
+  }, []);
+
   return (
     <Link className={styles.logo} to="/" onClick={scrollToTop}>
-      <svg className={styles.logoIcon}>
+      <svg ref={logoIconRef} className={styles.logoIcon}>
         <use
           xlinkHref={`${import.meta.env.BASE_URL}assets/icons/sprite.svg#logo`}
         />
