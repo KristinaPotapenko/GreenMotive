bca91a8376788cca8e4021704a00c41939fcc3bd
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-21 20:56:35 +0300

    feat: add GSAP reveal animation to DecorativeHeartIcon component

--- src/react/components/DecorativeHeartIcon/DecorativeHeartIcon.tsx
+++ src/react/components/DecorativeHeartIcon/DecorativeHeartIcon.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useRef } from "react";
 import styles from "./DecorativeHeartIcon.module.scss";
+import gsap from "gsap";
 
 export const DecorativeHeartIcon = () => {
+  const borderRef = useRef(null);
+
+  useEffect(() => {
+    if (!borderRef.current) return;
+
+    gsap.fromTo(
+      borderRef.current,
+      { scaleY: 0 },
+      {
+        scaleY: 1,
+        delay: 0.2,
+        duration: 1.5,
+        ease: "power2.out",
+        transformOrigin: "bottom",
+      }
+    );
+  }, []);
+
   return (
     <div className={styles.wrapper}>
+      <div ref={borderRef} className={styles.border}></div>
       <svg className={styles.icon}>
         <use
           xlinkHref={`${import.meta.env.BASE_URL}assets/icons/sprite.svg#heart`}
