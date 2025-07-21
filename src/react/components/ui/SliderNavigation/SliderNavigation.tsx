b8e811ab1015fb6335ae4d017c447cf509e765b7
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-21 20:55:29 +0300

    feat: add GSAP reveal animation to SliderNavigation component

--- src/react/components/ui/SliderNavigation/SliderNavigation.tsx
+++ src/react/components/ui/SliderNavigation/SliderNavigation.tsx
@@ -1,15 +1,39 @@
 import cl from "classnames";
 
 import styles from "./SliderNavigation.module.scss";
+import { useEffect } from "react";
+import gsap from "gsap";
 
 export const SliderNavigation = () => {
+  useEffect(() => {
+    const items = gsap.utils.toArray<HTMLElement>("[data-item]");
+    gsap.fromTo(
+      items,
+      { opacity: 0, y: 60 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+        stagger: 0.2,
+        ease: "power3.out",
+      }
+    );
+  }, []);
+
   return (
     <ul className={styles.navigation}>
-      <li className={cl(styles.navigationItem, styles.navigationItemActive)}>
+      <li
+        className={cl(styles.navigationItem, styles.navigationItemActive)}
+        data-item
+      >
         01
       </li>
-      <li className={styles.navigationItem}>02</li>
-      <li className={styles.navigationItem}>03</li>
+      <li className={styles.navigationItem} data-item>
+        02
+      </li>
+      <li className={styles.navigationItem} data-item>
+        03
+      </li>
     </ul>
   );
 };
