f45928588a023d164482408f02dd85ae62243c25
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-22 21:50:56 +0300

    feat: animate Footer component

--- src/react/components/layouts/Footer/Footer.tsx
+++ src/react/components/layouts/Footer/Footer.tsx
@@ -1,15 +1,46 @@
+import { useEffect, useRef } from "react";
 import cl from "classnames";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { Input } from "../../inputs/Input/Input";
 import { Logo } from "../../Logo/Logo";
 
 import styles from "./Footer.module.scss";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export const Footer = () => {
+  const footerRef = useRef(null);
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    if (!footerRef.current || !footerRef.current) return;
+
+    gsap.fromTo(
+      formRef.current,
+      {
+        y: 20,
+        opacity: 0,
+      },
+      {
+        scrollTrigger: {
+          trigger: footerRef.current,
+          start: "-30 bottom",
+          end: "bottom bottom",
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+      }
+    );
+  }, []);
+
   return (
-    <footer className={cl("container", styles.footer)}>
-      <Logo />
-      <form className={styles.footerForm}>
+    <footer ref={footerRef} className={cl("container", styles.footer)}>
+      <Logo section={footerRef} />
+      <form ref={formRef} className={styles.footerForm}>
         <Input
           id="email"
           type="email"
