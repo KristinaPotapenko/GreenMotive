74e56aa9074b8c0049b7b3a6a162100581875248
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-22 21:45:05 +0300

    feat: animate MoreLink with ScrollTrigger

--- src/react/components/ui/MoreLink/MoreLink.tsx
+++ src/react/components/ui/MoreLink/MoreLink.tsx
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 import gsap from "gsap";
 
 import styles from "./MoreLink.module.scss";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 interface MoreLinkProps {
   path: string;
+  section?: React.RefObject<HTMLElement | null>;
   children: React.ReactNode;
 }
 
-export const MoreLink = ({ path, children }: MoreLinkProps) => {
+gsap.registerPlugin(ScrollTrigger);
+
+export const MoreLink = ({ path, section, children }: MoreLinkProps) => {
   const linkRef = useRef(null);
 
   useEffect(() => {
@@ -21,6 +25,30 @@ export const MoreLink = ({ path, children }: MoreLinkProps) => {
         y: -40,
       },
       {
+        opacity: 1,
+        y: 0,
+        delay: 0.2,
+        duration: 1.5,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!section) return;
+    if (!section.current) return;
+
+    gsap.fromTo(
+      linkRef.current,
+      {
+        opacity: 0,
+        y: -40,
+      },
+      {
+        scrollTrigger: {
+          trigger: section.current,
+          start: "40% center",
+          toggleActions: "restart none none none",
+        },
         opacity: 1,
         y: 0,
         delay: 0.2,
