117cd3ab651d83ac99c6483f8a42527e75549969
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-23 21:20:25 +0300

    feat: add disappear animation (slide up) for Tag component

--- src/react/components/Tag/Tag.tsx
+++ src/react/components/Tag/Tag.tsx
@@ -1,31 +1,58 @@
+import React, { useEffect, useRef } from "react";
 import cl from "classnames";
 
-import styles from "./Tag.module.scss";
-import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+import styles from "./Tag.module.scss";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export type TagTheme = "Green" | "Dark" | "White";
 
 interface TagProps {
   label: string;
   theme: TagTheme;
+  section?: React.RefObject<HTMLElement | null>;
 }
 
-export const Tag = ({ label, theme }: TagProps) => {
+export const Tag = ({ label, theme, section }: TagProps) => {
   const tagRef = useRef(null);
 
   useEffect(() => {
     gsap.fromTo(
       tagRef.current,
       {
-        opacity: 0,
         y: 40,
+        opacity: 0,
       },
       {
-        opacity: 1,
         y: 0,
+        opacity: 1,
         delay: 0.2,
         duration: 1.5,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    if (!tagRef.current || !section?.current) return;
+
+    gsap.fromTo(
+      tagRef.current,
+      {
+        y: 0,
+        opacity: 1,
+      },
+      {
+        y: -40,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: section.current,
+          start: "center center",
+          end: "bottom center",
+          scrub: true,
+        },
       }
     );
   }, []);
