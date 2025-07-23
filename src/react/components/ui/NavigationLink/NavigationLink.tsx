d1b22990552216bc87a1f258720619bca8aa5b38
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-23 21:15:09 +0300

    feat: add hover and animation to MoreLink

--- src/react/components/ui/NavigationLink/NavigationLink.tsx
+++ src/react/components/ui/NavigationLink/NavigationLink.tsx
@@ -1,15 +1,51 @@
 import type React from "react";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
+
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import styles from "./NavigationLink.module.scss";
 
+gsap.registerPlugin(ScrollTrigger);
+
 interface NavigationLinkProps {
   path: string;
+  section?: React.RefObject<HTMLElement | null>;
   children: React.ReactNode;
 }
-export const NavigationLink = ({ path, children }: NavigationLinkProps) => {
+
+export const NavigationLink = ({
+  path,
+  section,
+  children,
+}: NavigationLinkProps) => {
+  const linkRef = useRef(null);
+
+  useEffect(() => {
+    if (!section) return;
+    if (!linkRef.current || !section.current) return;
+
+    gsap.fromTo(
+      linkRef.current,
+      {
+        y: 0,
+        opacity: 1,
+      },
+      {
+        scrollTrigger: {
+          trigger: section.current,
+          start: "60% center",
+          scrub: true,
+        },
+        y: -100,
+        opacity: 0,
+      }
+    );
+  }, []);
+
   return (
-    <Link to={path} className={styles.button}>
+    <Link ref={linkRef} to={path} className={styles.button}>
       {children}
     </Link>
   );
