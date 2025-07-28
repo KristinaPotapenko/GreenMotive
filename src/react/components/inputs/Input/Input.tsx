667f2406e79696b665c9f734a65fafbbc85e80c3
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-28 21:41:11 +0300

    refactor(Input): small improvements and cleanup

--- src/react/components/inputs/Input/Input.tsx
+++ src/react/components/inputs/Input/Input.tsx
@@ -1,13 +1,16 @@
+import React, { forwardRef } from "react";
+import cl from "classnames";
+
 import styles from "./Input.module.scss";
 
-interface InputProps {
-  id?: string;
-  type?: string;
-  name?: string;
-  pattern?: string;
-  placeholder?: string;
-}
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = (props: InputProps) => {
-  return <input className={styles.input} {...props} />;
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...rest }, ref) => {
+    return (
+      <input ref={ref} className={cl(styles.input, className)} {...rest} />
+    );
+  }
+);
+
+Input.displayName = "Input";
