895fee47debbfc27ee2a6b1f7ee6df36be69235e
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-29 10:23:23 +0300

    refactor(Logo): small improvements and cleanup

--- src/react/components/Logo/Logo.tsx
+++ src/react/components/Logo/Logo.tsx
@@ -7,6 +7,7 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { scrollToTop } from "../../../scripts/helpers/scrollToTop";
 
 import styles from "./Logo.module.scss";
+import { iconHref } from "../../../utils/constance";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -15,40 +16,40 @@ interface LogoProps {
 }
 
 export const Logo = ({ section }: LogoProps) => {
-  const logoRef = useRef(null);
-  const logoIconRef = useRef(null);
+  const logoRef = useRef<HTMLAnchorElement>(null);
+  const logoIconRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
+    if (!logoIconRef.current) return;
+
     gsap.to(logoIconRef.current, {
       opacity: 0.5,
       rotate: 180,
       delay: 0.2,
       duration: 2,
     });
-  }, []);
 
-  useEffect(() => {
-    if (!section?.current) return;
-
-    gsap.fromTo(
-      logoRef.current,
-      {
-        y: 40,
-        opacity: 0,
-      },
-      {
-        scrollTrigger: {
-          trigger: section.current,
-          start: "top bottom",
-          end: "bottom bottom",
-          scrub: true,
+    if (section?.current && logoRef.current) {
+      gsap.fromTo(
+        logoRef.current,
+        {
+          y: 40,
+          opacity: 0,
         },
-        y: 0,
-        opacity: 1,
-        delay: 0.2,
-        duration: 1.5,
-      }
-    );
+        {
+          scrollTrigger: {
+            trigger: section.current,
+            start: "top bottom",
+            end: "bottom bottom",
+            toggleActions: "play none none none",
+          },
+          y: 0,
+          opacity: 1,
+          delay: 0.2,
+          duration: 1.5,
+        }
+      );
+    }
   }, []);
 
   return (
@@ -59,9 +60,7 @@ export const Logo = ({ section }: LogoProps) => {
       onClick={scrollToTop}
     >
       <svg ref={logoIconRef} className={styles.logoIcon}>
-        <use
-          xlinkHref={`${import.meta.env.BASE_URL}assets/icons/sprite.svg#logo`}
-        />
+        <use xlinkHref={`${iconHref}logo`} />
       </svg>
       <p className={styles.logoText}>GreenMotive</p>
     </Link>
