a7085a682343012c154e29ebe8b8d83535d6d0d3
Kristina Potapenko <potapenkokristina10@gmail.com>
2025-07-29 11:50:27 +0300

    refactor(MoreLink): small improvements and cleanup

--- src/react/components/ui/MoreLink/MoreLink.tsx
+++ src/react/components/ui/MoreLink/MoreLink.tsx
@@ -20,11 +20,15 @@ export const MoreLink = ({
   section,
   children,
 }: MoreLinkProps) => {
-  const linkRef = useRef(null);
+  const linkRef = useRef<HTMLAnchorElement>(null);
 
   useEffect(() => {
+    const link = linkRef.current;
+
+    if (!link) return;
+
     gsap.fromTo(
-      linkRef.current,
+      link,
       {
         opacity: 0,
         y: -40,
@@ -36,56 +40,51 @@ export const MoreLink = ({
         duration: 1.5,
       }
     );
-  }, []);
 
-  useEffect(() => {
-    if (!section) return;
-    if (!section.current) return;
-    if (section.current.className.includes("hero")) return;
+    if (!section?.current) return;
+    const isHero = section.current.className.includes("hero");
 
-    gsap.fromTo(
-      linkRef.current,
-      {
-        opacity: 0,
-        y: -40,
-      },
-      {
-        scrollTrigger: {
-          trigger: section.current,
-          start: "50% center",
-          toggleActions: "restart none none none",
+    if (!isHero) {
+      gsap.fromTo(
+        link,
+        {
+          opacity: 0,
+          y: -40,
         },
-        opacity: 1,
-        y: 0,
-        delay: 0.2,
-        duration: 1.5,
-      }
-    );
-  }, []);
+        {
+          scrollTrigger: {
+            trigger: section.current,
+            start: "50% center",
+            toggleActions: "restart none none none",
+          },
+          opacity: 1,
+          y: 0,
+          delay: 0.2,
+          duration: 1.5,
+        }
+      );
+    }
 
-  useEffect(() => {
-    if (!section) return;
-    if (!section.current) return;
-    if (!section.current.className.includes("hero")) return;
-
-    gsap.fromTo(
-      linkRef.current,
-      {
-        y: 0,
-        opacity: 1,
-      },
-      {
-        scrollTrigger: {
-          trigger: section.current,
-          start: "70% center",
-          end: "bottom center",
-          scrub: true,
+    if (isHero) {
+      gsap.fromTo(
+        link,
+        {
+          y: 0,
+          opacity: 1,
         },
-        y: -60,
-        opacity: 0,
-      }
-    );
-  }, []);
+        {
+          scrollTrigger: {
+            trigger: section.current,
+            start: "70% center",
+            end: "bottom center",
+            scrub: true,
+          },
+          y: -60,
+          opacity: 0,
+        }
+      );
+    }
+  }, [section]);
 
   return (
     <Link ref={linkRef} to={path} className={styles.link}>
